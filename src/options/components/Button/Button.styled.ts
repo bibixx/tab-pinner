7ea96012dff58a180c3e5e7f5aa4f380bc0458dc
bibixx <bartosz@legiec.io>
2020-04-29 22:53:43 +0200

    refactor(options): add animations to modal

--- src/options/components/Button/Button.styled.ts
+++ src/options/components/Button/Button.styled.ts
@@ -4,7 +4,7 @@ const StyledButton = styled.button`
   border-radius: 0.25em;
   font-family: var(--font-family);
   font-weight: 500;
-  font-size: 0.875em;
+  font-size: 1em;
   padding: 0.5em 1em;
   border: 0;
   appearance: none;
--- src/options/components/Modal/Modal.tsx
+++ src/options/components/Modal/Modal.tsx
@@ -16,46 +16,73 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({
   isOpen, onClose, children, header, acceptButtonText,
-}) => (
-  <ClassNames>
-    {({ css }) => (
-      <ReactModal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        overlayClassName={css`
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: rgba(0, 0, 0, 0.4);
-        `}
-        className={css`
-          position: absolute;
-          display: inline-block;
-          width: 40rem;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          outline: 0;
-        `}
-      >
-        <Wrapper>
-          {header && (
-            <Header>
-              <StyledH2>{header}</StyledH2>
-            </Header>
-          )}
-          <Body>
-            {children}
-          </Body>
-          <Footer>
-            <Button onClick={onClose}>{acceptButtonText}</Button>
-          </Footer>
-        </Wrapper>
-      </ReactModal>
-    )}
-  </ClassNames>
-);
+}) => {
+  const { matches: prefersReducedMotion } = window.matchMedia('(prefers-reduced-motion: reduce)');
+  const animationTime = prefersReducedMotion ? 0 : 150;
+
+  return (
+    <ClassNames>
+      {({ css }) => (
+        <ReactModal
+          isOpen={isOpen}
+          onRequestClose={onClose}
+          closeTimeoutMS={animationTime}
+          overlayClassName={{
+            base: css`
+              position: fixed;
+              top: 0;
+              left: 0;
+              right: 0;
+              bottom: 0;
+              background-color: rgba(0, 0, 0, 0.4);
+              opacity: 0;
+              transition: opacity ${animationTime}ms ease-out;
+            `,
+            afterOpen: css`
+              opacity: 1 !important;
+            `,
+            beforeClose: css`
+              opacity: 0 !important;
+            `,
+          }}
+          className={{
+            base: css`
+              position: absolute;
+              display: inline-block;
+              width: 40rem;
+              top: 50%;
+              left: 50%;
+              outline: 0;
+              opacity: 0;
+              transform: translate(-50%, -50%);
+              transition: ${animationTime}ms ease-out;
+              transition-property: opacity, transition;
+            `,
+            afterOpen: css`
+              opacity: 1 !important;
+            `,
+            beforeClose: css`
+              opacity: 0 !important;
+            `,
+          }}
+        >
+          <Wrapper>
+            {header && (
+              <Header>
+                <StyledH2>{header}</StyledH2>
+              </Header>
+            )}
+            <Body>
+              {children}
+            </Body>
+            <Footer>
+              <Button onClick={onClose}>{acceptButtonText}</Button>
+            </Footer>
+          </Wrapper>
+        </ReactModal>
+      )}
+    </ClassNames>
+  );
+};
 
 export default Modal;
