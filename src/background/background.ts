07d551d80cc79451533baebda2229b4cc6a3094d
Bartosz Legięć <bartosz@legiec.io>
2022-08-03 10:05:27 +0200

    fix: fix event handlers not registering sometimes

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,15 +3,10 @@ import { onCreated } from './listeners/onCreated';
 import { onIconClick } from './listeners/onIconClick';
 import { onUpdate } from './listeners/onUpdate';
 
-function bindListeners() {
-  chrome.tabs.onUpdated.addListener(onUpdate);
+chrome.tabs.onUpdated.addListener(onUpdate);
 
-  chrome.tabs.onCreated.addListener(onCreated);
+chrome.tabs.onCreated.addListener(onCreated);
 
-  chrome.tabs.onActivated.addListener(onActivated);
+chrome.tabs.onActivated.addListener(onActivated);
 
-  chrome.action.onClicked.addListener(onIconClick);
-}
-
-chrome.runtime.onInstalled.addListener(bindListeners);
-chrome.runtime.onStartup.addListener(bindListeners);
+chrome.action.onClicked.addListener(onIconClick);
