e761e3ca955448aaf0c57d34ef0e697107ce4287
Bartosz Legięć <bartosz@legiec.io>
2022-08-05 18:42:09 +0200

    fix(icons): add 32x32 icon for better quality in top bar button

--- src/background/changeIcon.ts
+++ src/background/changeIcon.ts
@@ -6,6 +6,7 @@ const getProperties = (pinned: boolean) => {
       title: getTranslatedText('unpin'),
       path: {
         16: 'imgs/icon_in_16.png',
+        32: 'imgs/icon_in_32.png',
         48: 'imgs/icon_in_48.png',
         128: 'imgs/icon_in_128.png',
         256: 'imgs/icon_in_256.png',
@@ -17,6 +18,7 @@ const getProperties = (pinned: boolean) => {
     title: getTranslatedText('pin'),
     path: {
       16: 'imgs/icon_16.png',
+      32: 'imgs/icon_32.png',
       48: 'imgs/icon_48.png',
       128: 'imgs/icon_128.png',
       256: 'imgs/icon_256.png',
